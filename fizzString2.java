12fb320983553950505231a7f633ef20b2e229d1
Neil <cyberlionsystem@ymail.com>
2016-06-26 22:04:44 +0400

    Create fizzString2.java

---
+++ fizzString2.java
@@ -0,0 +1,29 @@
+// Given an int n, return the string form of the number followed by "!". 
+// So the int 6 yields "6!". 
+// Except if the number is divisible by 3 use "Fizz" instead of the number, and if the number is divisible by 5 use "Buzz", and if divisible by both 3 and 5, use "FizzBuzz". 
+// Note: the % "mod" operator computes the remainder after division, so 23 % 10 yields 3. 
+// What will the remainder be when one number divides evenly into another? 
+
+// fizzString2(1) → "1!"
+// fizzString2(2) → "2!"
+// fizzString2(3) → "Fizz!"
+
+public String fizzString2(int n) {
+  
+  //int n = 0;
+  String answer =n+"!";
+  
+  if ((n%3==0)&&(n%5==0)){
+    answer = "FizzBuzz!";
+  }
+  
+  else if (n%3==0) {
+    answer = "Fizz!";
+  }
+  
+  else if (n%5==0) {
+    answer = "Buzz!";
+  }
+  
+  return answer;
+}
